6364cbccb4aaba8999827f7b55328bc114660b81
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-10-21 11:48:23 +0530

    Added method for compare and some tweaks for HexCalc Class

--- 001 - Software Construction Fundamentals - GET/Chapter 002 - Static Checking and Code Review/Assignment 1/Assignment.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 002 - Static Checking and Code Review/Assignment 1/Assignment.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
 import java.util.HashMap;
-import java.util.Map;
 
 class BaseConverter {
 	private final HashMap<String, String> map = new HashMap<String, String>();
+	private final int CHAR_ASCII_SHIFT = 87;
 
 	public BaseConverter(){
 		map.put("0", "0");
@@ -47,25 +46,23 @@ class BaseConverter {
 	public int toDecimal(String number, int base){
 
 		// Exception Handling
-		if(number.isEmpty()) return 0;
-		if(base < 2) return 0;
+		if(number.isEmpty()) return -1;
+		if(base < 2) return -1;
 
 		String normalisedNumber = number.toLowerCase();
 
 		int numberLengthInText = normalisedNumber.length();
 		for(int i = 0; i<numberLengthInText; i++){
 			char c = normalisedNumber.charAt(i);
-			if(c == ' ') return 0;
+			if(c == ' ') return -1;
 
 			int numericValueOfChar = Integer.parseInt(map.get(c+""));
-			if(numericValueOfChar >= base) return 0;
+			if(numericValueOfChar >= base) return -1;
 		}
 		
 		// Main Logic
 		int result = 0;
-
 		for(int i = numberLengthInText-1; i>=0; i--){
-			System.out.println("hahdasd");
 			int lastDigit = Integer.parseInt(map.get(normalisedNumber.charAt(i)+""));
 			Double place_value = Math.pow(base, numberLengthInText - 1 - i);
 			int final_value = lastDigit * place_value.intValue();
@@ -74,13 +71,77 @@ class BaseConverter {
 
 		return result;
 	}
+
+	public String toBase(int number, int base){
+		if(base > 35) return "Invalid Base";
+		String result = "";
+		int numberCopy = number;
+		while(numberCopy > 0){
+			int remainder = numberCopy % base;
+			numberCopy = numberCopy / base;
+
+			if(remainder <= 9){
+				result = remainder + result;
+			}else{
+				result = result + Character.toString((char) remainder + CHAR_ASCII_SHIFT);
+			}
+		}
+
+		return result;
+	}
+}
+
+class HexCalc {
+	static BaseConverter bc = new BaseConverter();
+	public static String addNumber(String num1, String num2, int base1, int base2){
+		int num1_decimal = bc.toDecimal(num1, base1);
+		int num2_decimal = bc.toDecimal(num2, base1);
+		return bc.toBase(num1_decimal + num2_decimal, base2) + "";
+	}
+	public static String subtractNumber(String num1, String num2, int base1, int base2){
+		int num1_decimal = bc.toDecimal(num1, base1);
+		int num2_decimal = bc.toDecimal(num2, base1);
+		return bc.toBase(num1_decimal - num2_decimal, base2) + "";
+	}
+	public static String multiplyNumber(String num1, String num2, int base1, int base2){
+		int num1_decimal = bc.toDecimal(num1, base1);
+		int num2_decimal = bc.toDecimal(num2, base1);
+		return bc.toBase(num1_decimal * num2_decimal, base2) + "";
+	}
+	public static String divideNumber(String num1, String num2, int base1, int base2){
+		int num1_decimal = bc.toDecimal(num1, base1);
+		int num2_decimal = bc.toDecimal(num2, base1);
+		return bc.toBase(num1_decimal * num2_decimal, base2) + "";
+	}
+
+	// Prints 
+	// 1  if num1 > num2
+	// 0  if num1 = num2
+	// -1 if num2 < num2
+	public static int compare(String num1, String num2, int base){
+		int num1_decimal = bc.toDecimal(num1, base);
+		int num2_decimal = bc.toDecimal(num2, base);
+		if(num1_decimal < 0 || num2_decimal < 0) return -2;
+
+		if(num1.length() > num2.length()) return 1;
+		if(num1.length() < num2.length()) return -1;
+		if(num1.equals(num2)) return 0;
+
+		int len = num1.length();
+		for(int i = 0; i<len; i++){
+			if(num1.charAt(i) > num2.charAt(i)) return 1;
+			if(num1.charAt(i) < num2.charAt(i)) return -1;
+		}
+
+		return -2;
+	}
 }
 
 class Assignment {
 	public static void main(String[] args) {
 
-		BaseConverter bc = new BaseConverter();
-		System.out.println(bc.toDecimal("101", 2));
+		System.out.println(HexCalc.addNumber("1000", "228", 10, 16));
+		System.out.println(HexCalc.compare("4F", "4C", 16));
 		
 	}
 }
