00c13d9afae7cc4a028308ccfb8b9a8116c889d0
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-10-23 09:47:54 +0530

    prettified file

--- 001 - Software Construction Fundamentals - GET/Chapter 003 - Software Specifications/Assignment 2/Assignment.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 003 - Software Specifications/Assignment 2/Assignment.java
@@ -2,37 +2,41 @@ import java.util.ArrayList;
 
 class ClassMarksheet {
 	ArrayList<Integer> marksheet = new ArrayList<Integer>();
-	private final int PASSING_MARKS = 40; 
+	private final int PASSING_MARKS = 40;
 
 	/**
 	 * add marks to list
+	 * 
 	 * @param marks marks obtained by student
 	 * @throws ArithematicException when marks<0
 	 */
-	public void addMarks(int marks){
-		try{
-			if(marks<0 || marks>100) throw new ArithmeticException("Invalid Marks");
+	public void addMarks(int marks) {
+		try {
+			if (marks < 0 || marks > 100)
+				throw new ArithmeticException("Invalid Marks");
 			marksheet.add(marks);
-		}catch(ArithmeticException e){
+		} catch (ArithmeticException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
 	/**
 	 * get average of all marks
-	 * @return double average rounded to 2 decimal places 
+	 * 
+	 * @return double average rounded to 2 decimal places
 	 * @throws ArithematicException if Marksheet is empty
 	 */
-	public double averageMarks(){
-		try{
-			if(marksheet.isEmpty()) throw new ArithmeticException("Marksheet Empty");
+	public double averageMarks() {
+		try {
+			if (marksheet.isEmpty())
+				throw new ArithmeticException("Marksheet Empty");
 			double sum = 0;
-			for(Integer i : marksheet){
+			for (Integer i : marksheet) {
 				sum += i;
 			}
-			double result = sum/(double)marksheet.size();
+			double result = sum / (double) marksheet.size();
 			return Double.parseDouble(String.format("%.2f", result));
-		}catch(ArithmeticException e){
+		} catch (ArithmeticException e) {
 			System.out.println(e.getMessage());
 			return 0;
 		}
@@ -40,38 +44,44 @@ class ClassMarksheet {
 
 	/**
 	 * maximum marks obtained
+	 * 
 	 * @return int maximum marks in list
 	 * @throws ArithematicException if Marksheet is empty
 	 */
-	public int maximumMarks(){
-		try{
-			if(marksheet.isEmpty()) throw new ArithmeticException("Marksheet Empty");
+	public int maximumMarks() {
+		try {
+			if (marksheet.isEmpty())
+				throw new ArithmeticException("Marksheet Empty");
 			int max = 0;
-			for(Integer i : marksheet){
-				if(i > max) max = i;
+			for (Integer i : marksheet) {
+				if (i > max)
+					max = i;
 			}
 			return max;
-		}catch(ArithmeticException e){
+		} catch (ArithmeticException e) {
 			System.out.println(e.getMessage());
 			return 0;
 		}
-		
+
 	}
 
 	/**
 	 * minimum marks obtained
+	 * 
 	 * @return int minimum marks in list
 	 * @throws ArithematicException if Marksheet is empty
 	 */
-	public int minimumMarks(){
-		try{
-			if(marksheet.isEmpty()) throw new ArithmeticException("Marksheet Empty");
+	public int minimumMarks() {
+		try {
+			if (marksheet.isEmpty())
+				throw new ArithmeticException("Marksheet Empty");
 			int min = 100;
-			for(Integer i : marksheet){
-				if(i < min) min = i;
+			for (Integer i : marksheet) {
+				if (i < min)
+					min = i;
 			}
 			return min;
-		}catch(ArithmeticException e){
+		} catch (ArithmeticException e) {
 			System.out.println(e.getMessage());
 			return 0;
 		}
@@ -79,23 +89,25 @@ class ClassMarksheet {
 
 	/**
 	 * Percent of students who passed( marks > 40 )
+	 * 
 	 * @return double percentage rounded to 2 decimal places
 	 * @throws ArithematicException if Marksheet is empty
 	 */
-	public double percentOfPassedStudents(){
-		try{
-			if(marksheet.isEmpty()) throw new ArithmeticException("Marksheet Empty");
+	public double percentOfPassedStudents() {
+		try {
+			if (marksheet.isEmpty())
+				throw new ArithmeticException("Marksheet Empty");
 
 			double passedCount = 0;
-			for(Integer i : marksheet){
-				if(i >= PASSING_MARKS){
+			for (Integer i : marksheet) {
+				if (i >= PASSING_MARKS) {
 					passedCount += 1;
 				}
 			}
 
-			double ratio = passedCount/(double)marksheet.size();
-			return Double.parseDouble(String.format("%.2f", ratio*100));
-		}catch(ArithmeticException e){
+			double ratio = passedCount / (double) marksheet.size();
+			return Double.parseDouble(String.format("%.2f", ratio * 100));
+		} catch (ArithmeticException e) {
 			System.out.println(e.getMessage());
 			return 0;
 		}
@@ -115,7 +127,7 @@ class Assignment {
 		marksheet.addMarks(80);
 		marksheet.addMarks(90);
 		marksheet.addMarks(100);
-		
+
 		System.out.println(marksheet.averageMarks());
 		System.out.println(marksheet.maximumMarks());
 		System.out.println(marksheet.minimumMarks());
