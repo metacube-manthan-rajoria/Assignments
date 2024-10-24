69a54f3641c98089ecbf92907d1adfdff0b82724
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-10-24 15:25:38 +0530

    Wrote remaining tests

--- 001 - Software Construction Fundamentals - GET/Chapter 004 - Test Driven Development and Junit/AssignmentTest.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 004 - Test Driven Development and Junit/AssignmentTest.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AssignmentTest {
     int[][] intMatrix = {
-            { 5, 9, 4, 9, 4, 5 },
-            { 5, 4, 9, 4, 9, 5 },
-            { 1, 2, 3, 8, 9, 3, 2, 1 },
-            { 7, 1, 4, 9, 7, 4, 1 },
-            { 6, 5, 6, 4, 9, 7, 1, 4, 2, 7, 9, 2, 6, 5, 6 },
-            { 1, 2, 1, 4 },
-            { 1, 4, 5, 3, 5, 4, 1 },
+        { 5, 9, 4, 9, 4, 5 },
+        { 5, 4, 9, 4, 9, 5 },
+        { 1, 2, 3, 8, 9, 3, 2, 1 },
+        { 7, 1, 4, 9, 7, 4, 1 },
+        { 6, 5, 6, 4, 9, 7, 1, 4, 2, 7, 9, 2, 6, 5, 6 },
+        { 1, 2, 1, 4 },
+        { 1, 4, 5, 3, 5, 4, 1 },
 
-            { 1, 2, 2, 3, 4, 4 },
-            { 1, 1, 2, 1, 1, 4 },
-            { 1, 1, 1, 1, 1, 1 },
+        { 1, 2, 2, 3, 4, 4 },
+        { 1, 1, 2, 1, 1, 4 },
+        { 1, 1, 1, 1, 1, 1 },
 
-            { 1, 4, 1, 5 },
-            { 1, 4, 1, 5, 5, 4, 1 },
+        { 1, 4, 1, 5 },
+        { 1, 4, 1, 5, 5, 4, 1 },
 
-            { 1, 1, 1, 2, 1 },
-            { 2, 1, 1, 2, 1 },
-            { 10, 10 }
+        { 1, 1, 1, 2, 1 },
+        { 2, 1, 1, 2, 1 },
+        { 10, 10 }
     };
 
     private ArrayList<ArrayList<Integer>> intArrayIterator = new ArrayList<ArrayList<Integer>>();
@@ -94,6 +95,52 @@ public class AssignmentTest {
                 add(1);
                 add(1);
                 add(1);
+                add(1);
+            }
+        }, caseVerified);
+    }
+
+    @Test
+    public void runFixXY() {
+        for (int i = 0; i < intMatrix.length; i++) {
+            ArrayList<Integer> caseVerify = new ArrayList<Integer>();
+
+            for (int j = 0; j < intMatrix[i].length; j++) {
+                caseVerify.add(intMatrix[i][j]);
+            }
+
+            ArrOperation arrOperation = new ArrOperation(caseVerify);
+            int result[] = arrOperation.fixXY(4, 5);
+            assertArrayEquals(intMatrix[i], result);
+        }
+    }
+
+    @Test
+    public void runSplitArray() {
+        ArrayList<Integer> caseVerified = new ArrayList<Integer>();
+
+        for (ArrayList<Integer> testCase : intArrayIterator) {
+            ArrOperation arrOperation = new ArrOperation(testCase);
+            int result = arrOperation.splitArray();
+            caseVerified.add(result);
+        }
+
+        assertEquals(new ArrayList<Integer>() {
+            {
+                add(3);
+                add(3);
+                add(-1);
+                add(-1);
+                add(-1);
+                add(3);
+                add(-1);
+                add(4);
+                add(4);
+                add(3);
+                add(-1);
+                add(-1);
+                add(3);
+                add(-1);
                 add(1);
             }
         }, caseVerified);
