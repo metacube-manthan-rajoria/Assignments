5852a5be7f2cfb5386727b223f8bde9816d13db1
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-10-25 16:02:30 +0530

    IntSet now works

--- 001 - Software Construction Fundamentals - GET/Chapter 005 - Recursion/Assignment 2/AssignmentTest.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 005 - Recursion/Assignment 2/AssignmentTest.java
@@ -1,11 +1,12 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
+
 public class AssignmentTest {
-    RecursiveSearch rs = new RecursiveSearch();
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     final int NUMBER_TO_SEARCH = 7;
-
+    RecursiveSearch rs = new RecursiveSearch();
+    
     @Test
     public void linearSearchTest(){
         int linearResult = rs.linearSearch(arr, NUMBER_TO_SEARCH);
---
+++ 001 - Software Construction Fundamentals - GET/Chapter 006/Assignment.java
@@ -0,0 +1,99 @@
+import java.util.HashMap;
+final  class IntSet {
+    private final int array[];
+    private HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+
+    public IntSet(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            map.put(arr[i], 1);
+        }
+        array = arr;
+    }
+
+    public boolean isMember(int x){
+        if(map.getOrDefault(x, 0) == 1){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return map.size();
+    }
+
+    public boolean isSubSet(IntSet s){
+        HashMap<Integer, Integer> m = s.getSet();
+        boolean isSubSet = true;
+
+        for(Integer i : m.keySet()){
+            if(map.getOrDefault(i, 0) != 1){
+                isSubSet = false;
+            }
+        }
+
+        return isSubSet;
+    }
+
+    public int[] getComplement(){
+
+        int result[] = new int[1000 - map.size()];
+        int index = 0;
+        for(int i = 1; i<=1000; i++){
+            if(map.getOrDefault(i, 0) == 0){
+                result[index] = i;
+                index++;
+            }
+        }
+        return result;
+    }
+    
+    public int[] union(IntSet s){
+        HashMap<Integer, Integer> m = s.getSet();
+        
+        for(Integer i : map.keySet()){
+            m.put(i, 1);
+        }
+        
+        int result[] = new int[m.size()];
+        int index = 0;
+        for(Integer i : m.keySet()){
+            result[index] = i;
+            index++;
+        }
+
+        return result;
+    }
+
+    public void show(){
+        for(int i = 0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    public HashMap<Integer, Integer> getSet(){
+        return map;
+    }
+}
+
+public class Assignment{
+    
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,4,5};
+        int arr2[] = {3,4,7};
+        IntSet is1 = new IntSet(arr1);
+        IntSet is2 = new IntSet(arr2);
+
+        // System.out.println(is1.isMember(7));
+        // System.out.println(is1.size());
+        // System.out.println(is1.isSubSet(is2));
+
+        int a[] = is1.getComplement();
+        for(int i = 0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+        
+    }
+
+}
+
+
