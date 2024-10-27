e8b1aa011cf3e6e9f6790b1a7896c3411d013f42
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-10-27 23:31:22 +0530

    ADT assignment

---
+++ 001 - Software Construction Fundamentals - GET/Chapter 006/Assignment 2/Assignment.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+final class PolyOperation {
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    public PolyOperation(){
+        
+    }
+
+}
+
+public class Assignment{
+    
+    public static void main(String[] args) {
+
+        
+    }
+
+}
+
+
