903f918ac7e0b21249c768c0dfd7eee88918c4b1
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-02 16:42:21 +0530

    Removed demo code from Organization.java

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
@@ -25,18 +25,3 @@ public final class Organization {
         return result;
     }
 }
-
-class Assignment {
-    public static void main(String[] args) {
-        Department dept1 = new Department("Founders");
-        Department dept2 = new Department("Finance");
-        Department dept3 = new Department("Admin");
-
-        dept1.join("Head Arora Borealis", 100000, 20000);
-        dept2.join("Rajni Gandha", 40000, 10000);
-        dept2.join("Sastha Malik", 45000, 10000);
-        dept3.join("Swastik Agarwal", 15000, 10000);
-        dept3.join("Rahul Meena", 20000, 10000);
-        dept3.join("V1", 18000, 10000);
-    }
-}
