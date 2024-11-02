4edb171d0c420709a586e4bf2ea8bdab5b22f14f
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-02 16:50:22 +0530

    added empty name check for department

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Department.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Department.java
@@ -2,39 +2,53 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Department {
-    private static int idInitilizer = 0; 
+    private static int idInitilizer = 0;
     private final int departmentId;
     private final String departmentName;
     private List<Employee> employees;
 
-    public Department(String departmentName){
+    public Department(String departmentName) {
         this.departmentId = idInitilizer++;
         this.departmentName = departmentName;
-        employees = new ArrayList<Employee>();
+
+        if(!this.departmentName.equals("")){
+            employees = new ArrayList<Employee>();
+        }else{
+            idInitilizer--;
+        }
     }
 
-    private int addEmployee(String employeeName, double employeeSalary, double employeeBonus){
+    private int addEmployee(String employeeName, double employeeSalary, double employeeBonus) {
+        if ("".equals(employeeName) || employeeSalary < 0 || employeeBonus < 0) {
+            return -1;
+        }
+
         Employee employee = new Employee(employeeName, employeeSalary, employeeBonus, departmentId);
         employees.add(employee);
         return employees.size();
     }
-    public int join(String employeeName, double employeeSalary, double employeeBonus){
+
+    public int join(String employeeName, double employeeSalary, double employeeBonus) {
         return addEmployee(employeeName, employeeSalary, employeeBonus);
     }
 
-    private int removeEmployee(int employeeId){
+    private int removeEmployee(int employeeId) {
+        if (employeeId >= employees.size())
+            return -1;
+
         employees.remove(employeeId);
         return employees.size();
     }
-    public int relieve(int employeeId){
+
+    public int relieve(int employeeId) {
         return removeEmployee(employeeId);
     }
 
-    public String getDepartmentName(){
+    public String getDepartmentName() {
         return this.departmentName;
     }
-    
-    public List<Employee> getEmployees(){
+
+    public List<Employee> getEmployees() {
         List<Employee> employeesCopy = new ArrayList<Employee>();
         employeesCopy.addAll(employees);
         return employeesCopy;
