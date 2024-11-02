9fb41eb8f22d444f9652f5361d9ef1bae629a5e6
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-02 16:47:53 +0530

    Added invalid input checks

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/AssignmentTest.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/AssignmentTest.java
@@ -3,5 +3,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AssignmentTest {
-    
+    public void payoutTest(){
+        Department dept1 = new Department("Founders");
+        Department dept2 = new Department("Finance");
+        Department dept3 = new Department("Admin");
+
+        dept1.join("Head Arora Borealis", 100000, 20000);
+        dept2.join("Rajni Gandha", 40000, 10000);
+        dept2.join("Sastha Malik", 45000, 10000);
+        dept3.join("Swastik Agarwal", 15000, 10000);
+        dept3.join("Rahul Meena", 20000, 10000);
+        dept3.join("V1", 18000, 10000);
+    }
 }
