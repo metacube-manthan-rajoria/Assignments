5ab05beb15c47db06a516a5783932ca0a159a006
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-02 16:07:47 +0530

    Added demo code in assignment

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
@@ -13,6 +13,10 @@ public final class Organization {
         return departments.size();
     }
 
+    public Department getDepartment(int departmentId){
+        return departments.get(departmentId);
+    }
+
     List<Employee> getAllEmployees(){
         List<Employee> result = new ArrayList<>();
         for(Department department : departments){
@@ -24,9 +28,15 @@ public final class Organization {
 
 class Assignment {
     public static void main(String[] args) {
-        
+        Department dept1 = new Department("Founders");
+        Department dept2 = new Department("Finance");
+        Department dept3 = new Department("Admin");
 
-
-
+        dept1.join("Head Arora Borealis", 100000, 20000);
+        dept2.join("Rajni Gandha", 40000, 10000);
+        dept2.join("Sastha Malik", 45000, 10000);
+        dept3.join("Swastik Agarwal", 15000, 10000);
+        dept3.join("Rahul Meena", 20000, 10000);
+        dept3.join("V1", 18000, 10000);
     }
 }
