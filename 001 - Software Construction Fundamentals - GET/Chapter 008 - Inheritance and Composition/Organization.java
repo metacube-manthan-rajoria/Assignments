6c872a3d802e662f251e2d9e1cc6b5d0acc1632f
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-04 16:44:24 +0530

    Added a normal catch

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
@@ -1,39 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import Departments.Department;
+import Departments.ITDepartment;
+import Departments.ManagerialDepartment;
+import Departments.QADepartment;
+import Departments.HelperDepertment;
 import Employees.Employee;
+import Employees.Developer;
+import Employees.Manager;
+import Employees.Tester;
+import Employees.Helper;
 
 public final class Organization {
-    private final List<Department> departments;
+    private final HashMap<Department, Integer> departments;
 
     public Organization() {
-        departments = new ArrayList<Department>();
+        departments = new HashMap<Department, Integer>();
     }
 
-    public int addDepartment(Department department) {
-        if (!"".equals(department.getDepartmentName())) {
-            departments.add(department);
-        }
+    public boolean addDepartment(Department department) {
+        int isDepartmentAdded = departments.getOrDefault(department, 0);
+        if ("".equals(department.getDepartmentName()) || isDepartmentAdded == 1)
+            return false;
 
-        return departments.size();
+        departments.put(department, 1);
+        return true;
     }
 
-    public Department getDepartment(int departmentId) {
-        return departments.get(departmentId);
-    }
-
-    List<Employee> getAllEmployees() {
-        List<Employee> result = new ArrayList<>();
-        for (Department department : departments) {
-            result.addAll(department.getEmployees());
+    HashMap<Employee, Integer> getAllEmployees() {
+        HashMap<Employee, Integer> result = new HashMap<Employee, Integer>();
+        for (Department department : departments.keySet()) {
+            result.putAll(department.getEmployees());
         }
         return result;
     }
 
     public void printPaySlips() {
-        List<Employee> employeesList = getAllEmployees();
-        for (Employee employee : employeesList) {
+        HashMap<Employee, Integer> employeesList = getAllEmployees();
+        for (Employee employee : employeesList.keySet()) {
             String slip = new Payroll(employee).toString();
             System.out.println(slip + "\n");
         }
@@ -42,21 +46,37 @@ public final class Organization {
 
 class Main {
     public static void main(String[] args) {
-        try{
+        try {
             Organization org = new Organization();
 
-            Department dept1 = new Department("Finance");
-            Department dept2 = new Department("IT");
+            Department dept1 = new ITDepartment("IT");
+            Department dept2 = new ManagerialDepartment("Finance");
+            Department dept3 = new HelperDepertment("Finance");
+            Department dept4 = new QADepartment("Finance");
 
-            dept1.join("a", 400000,9);
-            dept2.join("b", 20000,9);
+            Employee e1 = new Developer("Raja Pari", 600000, 20000);
+            Employee e2 = new Developer("Hulla Hullare Hulle Hulle-Hulle", 1300000, 75000);
+            Employee e3 = new Manager("Shona", 1200000, 100000);
+            Employee e4 = new Tester("Alexandrovich Kasperov", 500000, 20000);
+            Employee e5 = new Helper("Mukesh Panwala", 300000, 10000);
+            Employee e6 = new Helper("Captain Marvel", 400000, 10000);
+
+            dept1.join(e1);
+            dept1.join(e2);
+            dept2.join(e3);
+            dept4.join(e4);
+            dept3.join(e5);
+            dept3.join(e6);
 
             org.addDepartment(dept1);
-            dept1.join("c", 70, 80);
             org.addDepartment(dept2);
-            
+            org.addDepartment(dept3);
+            org.addDepartment(dept4);
+
             org.printPaySlips();
         } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
