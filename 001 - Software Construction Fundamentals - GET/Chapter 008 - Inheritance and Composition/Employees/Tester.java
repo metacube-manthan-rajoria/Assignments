a0bcee3aaac896d210e8b81cc7f45615ac84bb56
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-04 13:01:23 +0530

    Added 4 child classes for employee

---
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Employees/Tester.java
@@ -0,0 +1,24 @@
+package Employees;
+
+public class Tester extends Employee{
+    private final double employeeSalary;
+    private final double employeeBonus;
+    
+    public Tester(String employeeName, double employeeSalary, double employeeBonus, int employeeDepartmentId){
+        super(employeeName, employeeDepartmentId);
+        this.employeeSalary = employeeSalary;
+        this.employeeBonus = employeeBonus;
+    }
+
+    public double getBasicSalary() {
+        return this.employeeSalary;
+    }
+
+    public double getBonus() {
+        return this.employeeBonus;
+    };
+
+    public double getCompensation() {
+        return this.employeeSalary + this.employeeBonus;
+    }
+}
