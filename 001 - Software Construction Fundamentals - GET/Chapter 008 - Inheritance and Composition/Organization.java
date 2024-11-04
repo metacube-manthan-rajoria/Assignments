b7e2fe33fc31e880346cd2141706798c46d9bbef
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-04 12:16:13 +0530

    Added demo code in psvm

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Organization.java
@@ -4,27 +4,57 @@ import java.util.List;
 public final class Organization {
     private final List<Department> departments;
 
-    public Organization(){
+    public Organization() {
         departments = new ArrayList<Department>();
     }
 
-    public int addDepartment(Department department){
-        if(!"".equals(department.getDepartmentName())){
+    public int addDepartment(Department department) {
+        if (!"".equals(department.getDepartmentName())) {
             departments.add(department);
         }
-        
+
         return departments.size();
     }
 
-    public Department getDepartment(int departmentId){
+    public Department getDepartment(int departmentId) {
         return departments.get(departmentId);
     }
 
-    List<Employee> getAllEmployees(){
+    List<Employee> getAllEmployees() {
         List<Employee> result = new ArrayList<>();
-        for(Department department : departments){
+        for (Department department : departments) {
             result.addAll(department.getEmployees());
         }
         return result;
+    }
+
+    public void printPaySlips() {
+        List<Employee> employeesList = getAllEmployees();
+        for (Employee employee : employeesList) {
+            String slip = new Payroll(employee).toString();
+            System.out.println(slip + "\n");
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        try{
+            Organization org = new Organization();
+
+            Department dept1 = new Department("Finance");
+            Department dept2 = new Department("IT");
+
+            dept1.join("a", 400000,9);
+            dept2.join("b", 20000,9);
+
+            org.addDepartment(dept1);
+            dept1.join("c", 70, 80);
+            org.addDepartment(dept2);
+            
+            org.printPaySlips();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
