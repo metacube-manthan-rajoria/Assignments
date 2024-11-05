ac9ecbad504a5bdce212e5bb4aace8fd0dcaacee
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-05 11:30:39 +0530

    Removed comments

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/AssignmentTest.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/AssignmentTest.java
@@ -62,31 +62,4 @@ public class AssignmentTest {
         Double output[] = {583333.33, 91666.66, 86666.66, 41166.66, 24541.66, 32458.34};
         assertArrayEquals(output, taxableIncomeArray);
     }
-
-    // private static Stream<Arguments> maxMirrorStream() {
-    //     return Stream.of(
-    //             Arguments.of(new int[] { 1, 2, 3, 8, 9, 3, 2, 1 }, 3),
-    //             Arguments.of(new int[] { 7, 1, 4, 9, 7, 4, 1 }, 2),
-    //             Arguments.of(new int[] { 6, 5, 6, 4, 9, 7, 1, 4, 2, 7, 9, 2, 6, 5, 6 }, 3),
-    //             Arguments.of(new int[] { 1, 2, 1, 4 }, 3),
-    //             Arguments.of(new int[] { 1, 4, 5, 3, 5, 4, 1 }, 7),
-    //             Arguments.of(new int[] { 1, 1, 2, 1, 1, 4 }, 3),
-    //             Arguments.of(new int[] { 1, 4, 1, 5 }, 3),
-    //             Arguments.of(new int[] { 1, 4, 1, 5, 5, 4, 1 }, 3),
-    //             Arguments.of(new int[] { 1, 1, 1, 2, 1 }, 3),
-    //             Arguments.of(new int[] { 10, 10 }, 2));
-    // }
-
-    // @ParameterizedTest
-    // @MethodSource("maxMirrorStream")
-    // public void maxMirrorTest(int arr[], int output) {
-    //     ArrayList<Integer> arrList = new ArrayList<Integer>();
-
-    //     for (int i = 0; i < arr.length; i++) {
-    //         arrList.add(arr[i]);
-    //     }
-
-    //     ArrOperation arrop = new ArrOperation(arrList);
-    //     assertEquals(output, arrop.maxMirror());
-    // }
 }
