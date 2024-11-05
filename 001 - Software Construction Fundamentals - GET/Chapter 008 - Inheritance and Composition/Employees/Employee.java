c3e2b54147a502368e4cc2a26fee092f87cf78a8
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-05 11:26:22 +0530

    Added comparable interface to employees

--- 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Employees/Employee.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 008 - Inheritance and Composition/Employees/Employee.java
@@ -1,6 +1,6 @@
 package Employees;
 
-abstract public class Employee {
+abstract public class Employee implements Comparable<Employee>{
     private static int idInitilizer = 0;
     private final int employeeId;
     private final String employeeName;
@@ -23,4 +23,9 @@ abstract public class Employee {
     abstract public double getBonus();
 
     abstract public double getCompensation();
+
+    @Override
+    public int compareTo(Employee other) {
+        return employeeId - other.getId();
+    }
 }
