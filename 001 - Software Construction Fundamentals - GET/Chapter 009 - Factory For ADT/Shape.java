e32c48a576f7d4e5f5ed88a65dd4b04f351db786
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-05 15:34:26 +0530

    Added basic shape classes

---
+++ 001 - Software Construction Fundamentals - GET/Chapter 009 - Factory For ADT/Shape.java
@@ -0,0 +1,14 @@
+public interface Shape {
+    enum ShapeType{
+        TRIANGLE,
+        RECTANGLE,
+        POLYGON,
+        CIRCLE
+    }
+
+    public double getArea();
+    public double getPerimeter();
+    public double getOrigin();
+    public double isPointEnclosed(Point p);
+    public double isPointEnclosed(long x, long y);
+}
---
+++ 001 - Software Construction Fundamentals - GET/Chapter 009 - Factory For ADT/Triangle.java
@@ -0,0 +1,24 @@
+public class Triangle implements Shape{
+    private Point p;
+    private long side = 0;
+
+    public Triangle(){
+
+    }
+
+    public double getArea(){
+        return 0.0;
+    }
+    public double getPerimeter(){
+        return 0.0;
+    }
+    public double getOrigin(){
+        return 0.0;
+    }
+    public double isPointEnclosed(Point p){
+        return 0.0;
+    }
+    public double isPointEnclosed(long x, long y){
+        return 0.0;
+    }
+}
