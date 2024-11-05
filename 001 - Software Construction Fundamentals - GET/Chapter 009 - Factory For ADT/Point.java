e3c5a89012d80cce07beabcefd96b52daa931ed0
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-05 14:28:01 +0530

    Added basic functionality for Point class

---
+++ 001 - Software Construction Fundamentals - GET/Chapter 009 - Factory For ADT/Point.java
@@ -0,0 +1,40 @@
+
+public class Point {
+    private long x = 0;
+    private long y = 0;
+
+    private final long ORIGIN_X = 0;
+    private final long ORIGIN_Y = 0;
+
+    public Point(long x, long y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX(){
+        return x;
+    }
+
+    public long getY(){
+        return y;
+    }
+    
+    public double distanceFrom(Point p){
+        long xDifference = p.getX() - x;
+        long yDifference = p.getY() - y;
+
+        double distance = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+        return distance;
+    }
+    public double distanceFrom(long x, long y){
+        long xDifference = x - this.x;
+        long yDifference = y - this.y;
+
+        double distance = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+        return distance;
+    }
+
+    public double distanceFromOrigin(){
+        return distanceFrom(ORIGIN_X, ORIGIN_Y);
+    }
+}
