ee5aa9ab8b7fe7dba8a7bc7ca06b58f73c49d8a1
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-06 21:07:51 +0530

    Made options dynamic

--- 001 - Software Construction Fundamentals - GET/Chapter 009 - Factory For ADT/Main.java
+++ 001 - Software Construction Fundamentals - GET/Chapter 009 - Factory For ADT/Main.java
@@ -1,5 +1,4 @@
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -22,16 +21,86 @@ public class Main {
             }
 
             Screen screen = new Screen(xMax, yMax);
-            screen.addShape(Shape.ShapeType.POLYGON);
 
-            //Delete this later
-            System.out.println("P : " + screen.getShape(0).getPerimeter());
-            System.out.println("A : " + screen.getShape(0).getArea() + "\n");
-            System.out.println("TS : " + screen.getShape(0).getTimestamp() + "\n");
+            int userSelection = 0;
+            while(userSelection >= 0){
+                System.out.println(
+                    "\nOperations ->\n" +
+                    "0. Exit\n" +
+                    "1. Add Shape\n" +
+                    "2. Get Shape Attributes\n" +
+                    "3. Delete Shape\n" +
+                    "4. Delete all Shapes of certain types\n" +
+                    ""
+                );
 
-    
+                userSelection = scanner.nextInt();
+
+                if(userSelection <= 0){
+                    break;
+                }else if(userSelection == 1){
+                    System.out.println(
+                        "\tEnter type of shape to add ->\n" +
+                        "\t0. Triangle\n" +
+                        "\t1. Rectangle\n" +
+                        "\t2. Polygon\n" +
+                        "\t3. Circle\n"
+                    );
+
+                    int userShapeSelection = 0;
+                    userShapeSelection = scanner.nextInt();
+
+                    if(userShapeSelection == 0){
+                        screen.addShape(Shape.ShapeType.TRIANGLE);
+                    }else if(userShapeSelection == 1){
+                        screen.addShape(Shape.ShapeType.RECTANGLE);
+                    }else if(userShapeSelection == 2){
+                        screen.addShape(Shape.ShapeType.POLYGON);
+                    }else{
+                        screen.addShape(Shape.ShapeType.CIRCLE);
+                    }
+                }else if(userSelection == 2){
+                    System.out.print("Enter shape index : ");
+                    int selectedShapeIndex = 0;
+                    selectedShapeIndex = scanner.nextInt();
+
+                    System.out.println(
+                        "\tEnter type of attribute ->\n" +
+                        "\t0. Perimeter\n" +
+                        "\t1. Area\n" +
+                        "\t2. Timestamp\n" +
+                        "\t3. Origin\n"
+                    );
+
+                    int userAttributeSelection = 0;
+                    userAttributeSelection = scanner.nextInt();
+
+                    Shape selectedShape = screen.getShape(selectedShapeIndex);
+                    if(userAttributeSelection == 0){
+                        System.out.println("Perimeter : " + selectedShape.getPerimeter());
+                    }else if(userAttributeSelection == 1){
+                        System.out.println("Area : " + selectedShape.getArea());
+                    }else if(userAttributeSelection == 2){
+                        System.out.println("Timestamp : " + selectedShape.getTimestamp());
+                    }else{
+                        Point origin = selectedShape.getOrigin();
+                        System.out.println("Origin : ( " + origin.getX() + ", " + origin.getY() + " )");
+                    }
+                }else if(userSelection == 3){
+                    System.out.print("Enter shape index to delete: ");
+                    int selectedShapeIndex = 0;
+                    selectedShapeIndex = scanner.nextInt();
+                    screen.deleteShape(selectedShapeIndex);
+                }else if(userSelection == 4){
+                    System.out.print("Enter shape Type to delete: ");
+                    int selectedShapeIndex = 0;
+                    selectedShapeIndex = scanner.nextInt();
+                    screen.deleteShape(selectedShapeIndex);
+                }else{
+                    continue;
+                }
+            }
             
-
             scanner.close();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
