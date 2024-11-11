f3fa16837c992d47358e4b0e42a8d5a31517d6a1
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-11 12:13:52 +0530

    Added remaining columns

--- 002 - DBMS/Chapter 002 - SQL Commands & Joins/StoreFront - Creation.sql
+++ 002 - DBMS/Chapter 002 - SQL Commands & Joins/StoreFront - Creation.sql
@@ -1,8 +1,8 @@
-create database store_front;
+CREATE DATABASE store_front;
 
-use store_front;
+USE store_front;
 
-create table users(
+CREATE TABLE users(
 	user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     first_name VARCHAR(100) NOT NULL, 
     middle_name VARCHAR(100), 
@@ -11,32 +11,70 @@ create table users(
     user_type VARCHAR(20) NOT NULL
 );
 
-create table products(
+CREATE TABLE categories(
+	category_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    parent_category_id INT
+);
+
+CREATE TABLE products(
 	product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     name VARCHAR(200) NOT NULL, 
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
     category INT,
     price DECIMAL(10,2) NOT NULL,
-    stock INT NOT NULL
+    stock INT NOT NULL,
+    FOREIGN KEY (category) REFERENCES categories(category_id)
 );
 
-create table images(
+CREATE TABLE images(
 	product_id INT NOT NULL, 
-    product_url VARCHAR(1000) NOT NULL
+    product_url VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
-create table addresses(
-	user_id INT NOT NULL, 
-    address_line1 VARCHAR(100) NOT NULL, 
-    address_line2 VARCHAR(100), 
-    zipcode VARCHAR(15) NOT NULL
-);
-
-create table zipcodes(
-	code_id VARCHAR(15) NOT NULL PRIMARY KEY,
+CREATE TABLE zipcodes(
+	zipcode_id VARCHAR(15) NOT NULL PRIMARY KEY,
 	city VARCHAR(170) NOT NULL,
     state VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE addresses(
+	address_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL, 
+    address_line1 VARCHAR(100) NOT NULL, 
+    address_line2 VARCHAR(100), 
+    zipcode VARCHAR(15) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (zipcode) REFERENCES zipcodes(zipcode_id)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL, 
+    order_date DATETIME NOT NULL,
+    status ENUM(
+        'shipping',
+        'returned',
+        'delivered',
+        'cancelled'
+    ),
+    address_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (address_id) REFERENCES addresses(address_id)
+);
+
+CREATE TABLE order_products (
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+
+
+
 
 
 
