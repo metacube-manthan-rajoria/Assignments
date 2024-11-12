d709963db83136cc02599a44b7ddd9427900fc13
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-13 00:16:24 +0530

    Added delete code

--- 002 - DBMS/Chapter 003 - JDBC/JDBC/app/src/main/java/jdbc/App.java
+++ 002 - DBMS/Chapter 003 - JDBC/JDBC/app/src/main/java/jdbc/App.java
@@ -10,17 +10,23 @@ public class App {
 
             System.out.print("Enter userId to search for orders : ");
             int userId1 = scanner.nextInt();
+            System.out.print("Enter productId to add 5 images : ");
+            int productId1 = scanner.nextInt();
+
             Records userOrders1 = storeFront.getUserOrders(
                 userId1,
                 StoreFront.OrderStatus.DELIVERED
             );
             userOrders1.printRecords();
 
+            storeFront.addImagesToProduct(productId1);
+
+            int deletedRecords = storeFront.deleteUnpopularProducts("2023-10-07");
+            System.out.println("Deleted records : " + deletedRecords);
+
             scanner.close();
         } catch (Exception e) {
             System.out.println("We ran into a error :\n" + e.getMessage());
         }
     }
-
-    
 }
