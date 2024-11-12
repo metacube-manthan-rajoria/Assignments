c81cd5cb9d2f5ec0e754665bf7f85023103c8975
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-11-12 19:58:52 +0530

    added q2,3,4 for task3

--- 002 - DBMS/Chapter 002 - SQL Commands & Joins/StoreFront_03_Queries.sql
+++ 002 - DBMS/Chapter 002 - SQL Commands & Joins/StoreFront_03_Queries.sql
@@ -44,21 +44,43 @@ FROM products
 WHERE products.stock < 50;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+/* Task 3 - Query 1
+Display Recent 50 Orders placed (Id, Order Date, Order Total).
+*/
 SELECT orders.order_id, orders.order_date, SUM(products.price * order_products.quantity)
 FROM orders
 JOIN order_products ON orders.order_id = order_products.order_id
 JOIN products ON order_products.product_id = products.product_id
 GROUP BY orders.order_id
-ORDER BY orders.order_id;
+ORDER BY orders.order_id DESC
+LIMIT 50;
+
+/* Task 3 - Query 2
+Display 10 most expensive Orders.
+*/
+SELECT orders.order_id, SUM(products.price * order_products.quantity) as Total
+FROM orders
+JOIN order_products ON orders.order_id = order_products.order_id
+JOIN products ON order_products.product_id = products.product_id
+GROUP BY orders.order_id
+ORDER BY 2 DESC
+LIMIT 10;
+
+/* Task 3 - Query 3
+Display all the Orders which are placed more than 10 days old 
+and one or more items from those orders are still not shipped.
+*/
+SELECT orders.order_id, orders.order_date 
+FROM orders
+WHERE orders.order_date < "2023-10-10" 
+AND orders.status = "shipping";
+
+/* Task 3 - Query 4
+Display list of shoppers which haven't ordered anything since last month.
+*/
+SELECT users.user_id, users.first_name, users.last_name, orders.order_date
+FROM users
+JOIN orders ON users.user_id = orders.user_id
+WHERE orders.order_date < "2023-11-01"
+AND users.user_type = "customer";
+
