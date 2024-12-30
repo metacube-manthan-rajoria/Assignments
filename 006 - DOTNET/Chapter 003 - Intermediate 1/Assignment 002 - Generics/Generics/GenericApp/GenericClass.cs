d4e4a192343147de36830dcb965206a71e7edead
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-30 23:39:43 +0530

    Added code for generics

---
+++ 006 - DOTNET/Chapter 003 - Intermediate 1/Assignment 002 - Generics/Generics/GenericApp/GenericClass.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericApp;
+
+public class GenericClass<T>
+{
+    public static void Compare(T val1, T val2){
+        bool isEqual = val1.Equals(val2);
+        Console.WriteLine(isEqual);
+    }
+}
+
+public class CheckList {
+    public static void IsListEmpty<T>(List<T> list){
+        if(list.Count > 0){
+            Console.WriteLine("Not Empty");
+        }else{
+            Console.WriteLine("Empty");
+        }
+    }
+}
---
+++ 006 - DOTNET/Chapter 003 - Intermediate 1/Assignment 002 - Generics/Generics/GenericConsole/Console.cs
@@ -0,0 +1,19 @@
+﻿using GenericApp;
+
+class Console
+{
+    static void Main(string[] args)
+    {
+        GenericClass<int>.Compare(1, 2);
+        GenericClass<int>.Compare(2, 2);
+        GenericClass<string>.Compare("hello", "hell");
+        GenericClass<string>.Compare("hello", "hello");
+
+        List<int> iList = new List<int>();
+        iList.Add(1);
+        List<string> sList = new List<string>();
+
+        CheckList.IsListEmpty(iList);
+        CheckList.IsListEmpty(sList);
+    }
+}
