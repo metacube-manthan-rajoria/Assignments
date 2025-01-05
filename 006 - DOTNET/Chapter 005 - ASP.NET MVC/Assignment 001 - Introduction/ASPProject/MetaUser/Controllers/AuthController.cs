771cc5026e6521d9705ec30dba42b7098f768265
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-05 16:47:40 +0530

    tried viewData

--- 006 - DOTNET/Chapter 005 - ASP.NET MVC/Assignment 001 - Introduction/ASPProject/MetaUser/Controllers/AuthController.cs
+++ 006 - DOTNET/Chapter 005 - ASP.NET MVC/Assignment 001 - Introduction/ASPProject/MetaUser/Controllers/AuthController.cs
@@ -7,20 +7,40 @@ namespace MetaUser.Controllers;
 public class AuthController : Controller
 {
     private readonly ILogger<AuthController> _logger;
+    public List<User> users = new List<User>{
+        new User{Username="Gamedemons", Email="gamedemons55@gmail.com", Password="12345678", ConfirmPassword="12345678"},
+        new User{Username="Void", Email="void@proton.me", Password="12345678", ConfirmPassword="12345678"},
+        new User{Username="Manthan", Email="manthanrajoria@hotmail.com", Password="12345678", ConfirmPassword="12345678"},
+        new User{Username="Nyx", Email="nyxane@gmail.com", Password="12345678", ConfirmPassword="12345678"}
+    };
 
     public AuthController(ILogger<AuthController> logger)
     {
         _logger = logger;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         return View("Register");
     }
 
+    [HttpPost]
+    public IActionResult Index(User user){
+        users.Add(user);
+        ViewData["usersList"] = users; 
+        return RedirectToAction("Index", "Users");
+    }
+
     public IActionResult Login()
     {
         return View();
+    }
+
+    public IActionResult AddUser(User user){
+        var users = (List<User>)ViewData["usersList"]!;
+        users.Add(user);
+        return RedirectToAction("Index", "Users");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- 006 - DOTNET/Chapter 005 - ASP.NET MVC/Assignment 001 - Introduction/ASPProject/MetaUser/Models/User.cs
+++ 006 - DOTNET/Chapter 005 - ASP.NET MVC/Assignment 001 - Introduction/ASPProject/MetaUser/Models/User.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MetaUser.Models;
 
 public class User
 {
     [Key]
     public int Id {get; set;}
+    [Required]
     public required string Username {get; set;}
+    [Required]
     public required string Email {get; set;}
+    [Required]
     public required string Password {get; set;}
+    [Required]
     public required string ConfirmPassword {get; set;}
 }
