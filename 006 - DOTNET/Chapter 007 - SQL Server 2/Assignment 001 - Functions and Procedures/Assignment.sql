339b8f0214ecb19dc2bec377caf9ee87c103c030
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-21 17:27:14 +0530

    Added triggers and temp table

---
+++ 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 001 - Functions and Procedures/Assignment.sql
@@ -0,0 +1,40 @@
+-- Function
+CREATE FUNCTION checkBookDate(@borrowDate DATE, @returnDate DATE)
+RETURNS INT
+BEGIN
+	DECLARE @datevalid INT = 0
+	IF @returnDate > @borrowDate
+		BEGIN
+			SET @datevalid = 1
+		END
+	ELSE
+		BEGIN
+			SET @datevalid = 0
+		END
+	RETURN @datevalid
+END
+
+-- Using Function
+SELECT [dbo].[checkBookDate](BorrowDate, '2025-10-09') FROM Borrowing;
+
+-- Procedure
+CREATE PROCEDURE updateBookReturnDate @id INT, @date DATE
+AS
+BEGIN
+	DECLARE @valid INT
+	SELECT @valid = [dbo].[checkBookDate](BorrowDate, @date) FROM Borrowing WHERE BorrowID = @id
+	IF @valid = 1
+		BEGIN
+			UPDATE Borrowing SET ReturnDate = @date WHERE BorrowID = @id
+		END
+	ELSE
+		BEGIN
+			UPDATE Borrowing SET ReturnDate = null WHERE BorrowID = @id
+		END
+END
+
+-- Using Procedure
+EXEC updateBookReturnDate @id = 15, @date = '2025-01-20';
+
+
+
