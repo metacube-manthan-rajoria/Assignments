c4aa4a34f773d25aafc24603054a960ffcf7a3f7
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-22 10:25:07 +0530

    Updated temp table code

--- 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 002 - Temp Table/Assignment.sql
+++ 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 002 - Temp Table/Assignment.sql
@@ -1,14 +1,53 @@
-CREATE TABLE #demoTemp(
-	BookID INT PRIMARY KEY IDENTITY(1,1), 
-	Title VARCHAR(100) NOT NULL,
-	Author VARCHAR(100) NOT NULL,
-	PublishedYear INT NOT NULL,
-	Category VARCHAR(50)
+CREATE TABLE #demoTempTable(
+	BorrowID INT PRIMARY KEY IDENTITY(1,1),
+	MemberID INT,
+	BookID INT,
+	BorrowDate DATE,
+	ReturnDate DATE
 );
 
+INSERT INTO #demoTempTable(MemberID, BookID, BorrowDate, ReturnDate) VALUES (1, 5, '2024-12-10', '2024-12-12');
+INSERT INTO #demoTempTable(MemberID, BookID, BorrowDate, ReturnDate) VALUES (1, 3, '2024-12-10', '2025-01-08');
+INSERT INTO #demoTempTable(MemberID, BookID, BorrowDate, ReturnDate) VALUES (4, 2, '2023-09-23', '2023-10-14');
+INSERT INTO #demoTempTable(MemberID, BookID, BorrowDate, ReturnDate) VALUES (3, 4, '2010-03-19', '2019-07-20');
+INSERT INTO #demoTempTable(MemberID, BookID, BorrowDate, ReturnDate) VALUES (2, 2, '2023-10-29', '2024-11-03');
 
-INSERT INTO #demoTemp(Title, Author, PublishedYear, Category) VALUES ('Thinking Fast and Slow', 'Daniel Kahneman', 2011, 'Psycology');
-INSERT INTO #demoTemp(Title, Author, PublishedYear, Category) VALUES ('Shadow Slave', 'Guilty Three', 2022, 'Fantasy');
-INSERT INTO #demoTemp(Title, Author, PublishedYear, Category) VALUES ('The Fault in our Stars', 'John Green', 2010, 'Romance Novel');
-INSERT INTO #demoTemp(Title, Author, PublishedYear, Category) VALUES ('Ikigai', 'Daniel Kahneman', 2016, 'Psycology');
-INSERT INTO #demoTemp(Title, Author, PublishedYear, Category) VALUES ('Fifty Stands', 'Deepandra', 2015, 'Action');
+use tempdb;
+
+-- Function
+CREATE FUNCTION checkBookDate(@borrowDate DATE, @returnDate DATE)
+RETURNS INT
+BEGIN
+	DECLARE @datevalid INT = 0
+	IF @returnDate > @borrowDate
+		BEGIN
+			SET @datevalid = 1
+		END
+	ELSE
+		BEGIN
+			SET @datevalid = 0
+		END
+	RETURN @datevalid
+END
+
+-- Using Function
+SELECT [dbo].[checkBookDate](BorrowDate, '2025-10-09') FROM Borrowing;
+
+-- Procedure
+CREATE PROCEDURE updateBookReturnDate @id INT, @date DATE
+AS
+BEGIN
+	DECLARE @valid INT
+	SELECT @valid = [dbo].[checkBookDate](BorrowDate, @date) FROM Borrowing WHERE BorrowID = @id
+	IF @valid = 1
+		BEGIN
+			UPDATE Borrowing SET ReturnDate = @date WHERE BorrowID = @id
+		END
+	ELSE
+		BEGIN
+			UPDATE Borrowing SET ReturnDate = null WHERE BorrowID = @id
+		END
+END
+
+-- Using Procedure
+EXEC updateBookReturnDate @id = 15, @date = '2025-01-20';
