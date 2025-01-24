96da61f4be0cdd8d1cedfd4ff44538b4fbb606a4
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-24 15:10:55 +0530

    Added aggregate queries

---
+++ 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 007 - Aggregate Functions/Assignment.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(Id) FROM Categories;
+
+SELECT SUM(DisplayOrder) FROM Categories;
+
+SELECT AVG(DisplayOrder) FROM Categories;
+
+SELECT MAX(DisplayOrder) FROM Categories;
+
+SELECT MIN(DisplayOrder) FROM Categories;
