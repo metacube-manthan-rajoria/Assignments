663b86067348d3a85f1bab40ba21a1b868d8520f
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-28 16:16:23 +0530

    Rewrote queries

--- 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 007 - Aggregate Functions/Assignment.sql
+++ 006 - DOTNET/Chapter 007 - SQL Server 2/Assignment 007 - Aggregate Functions/Assignment.sql
@@ -1,9 +1,36 @@
-SELECT COUNT(Id) FROM Categories;
+CREATE TABLE Employee(Id INT, Name VARCHAR(100),Age INT, Salary DECIMAL(10,2), JoiningDate DATE);
 
-SELECT SUM(DisplayOrder) FROM Categories;
+INSERT INTO Employee VALUES(1, 'Sam', 45, 10000, '2020-09-12');
+INSERT INTO Employee VALUES(2, 'Joey', 21, 25000, '2021-09-12');
+INSERT INTO Employee VALUES(3, 'Dee', 61, 30000, '2020-01-21');
+INSERT INTO Employee VALUES(4, 'Halo', 37, 45000, '2019-05-06');
+INSERT INTO Employee VALUES(5, 'Max', 26, 8000, '2021-11-01');
+INSERT INTO Employee VALUES(6, 'Razor', 43, 50000, '2021-03-08');
+INSERT INTO Employee VALUES(7, 'Finch', 39, 22000, '2021-06-15');
+INSERT INTO Employee VALUES(8, 'Chief', 32, 19000, '2023-08-18');
+INSERT INTO Employee VALUES(9, 'Don', 28, 25000, '2024-09-23');
+INSERT INTO Employee VALUES(10, 'John', 27, 25000, '2024-09-23');
 
-SELECT AVG(DisplayOrder) FROM Categories;
+SELECT * FROM Employee;
 
-SELECT MAX(DisplayOrder) FROM Categories;
+-- Employee having slary greater than avg salary
+SELECT * FROM Employee WHERE Salary > (SELECT AVG(SALARY) FROM Employee);
 
-SELECT MIN(DisplayOrder) FROM Categories;
+-- Count of people in each salary range
+SELECT Salary, COUNT(Salary) AS 'People Count' FROM Employee GROUP BY Salary;
+
+-- Show the employees with most number of similar salaries
+WITH peopleInSalaryBracket AS (
+	SELECT Salary, COUNT(Salary) AS PeopleCount FROM Employee GROUP BY Salary
+)
+SELECT * FROM Employee WHERE Salary IN (
+	SELECT Salary FROM peopleInSalaryBracket WHERE PeopleCount IN (
+		SELECT MAX(PeopleCount) FROM peopleInSalaryBracket
+	)
+);
+
+-- Sum of salaries of employees grouped according to year
+SELECT YEAR(JoiningDate), SUM(Salary) AS AccumulativeSalary FROM Employee GROUP BY YEAR(JoiningDate)
+
+-- Employees from which year earn the least on avg
+SELECT YEAR(JoiningDate), AVG(Salary) AS AvgSalary FROM Employee GROUP BY YEAR(JoiningDate)
